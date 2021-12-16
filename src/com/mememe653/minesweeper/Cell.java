a26969affb5ad199236d199886c474a6e7f1dcd9
mememe653 <95522491+mememe653@users.noreply.github.com>
2021-12-16 16:02:30 +1030

    Make board responsive to mouse clicks

--- src/com/mememe653/minesweeper/Cell.java
+++ src/com/mememe653/minesweeper/Cell.java
@@ -2,9 +2,11 @@ package com.mememe653.minesweeper;
 
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.event.MouseEvent;
 
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
+import javax.swing.event.MouseInputAdapter;
 
 public class Cell extends JPanel {
 	
@@ -12,22 +14,47 @@ public class Cell extends JPanel {
 	
 	private final int row;
 	private final int col;
+	private boolean uncovered = false;
 	
-	private ImageIcon ii = new ImageIcon("src/com/mememe653/resources/facingDown.png");
-
+	private ImageIcon facingDownII = new ImageIcon("src/com/mememe653/resources/facingDown.png");
+	private ImageIcon uncoveredII = new ImageIcon("src/com/mememe653/resources/0.png");
+	
+	private MouseListener mouseListener = new MouseListener();
+	
 	public Cell(int row, int col) {
 		this.row = row;
 		this.col = col;
 		
-		Image img = ii.getImage().getScaledInstance(WIDTH, WIDTH, Image.SCALE_DEFAULT);
-		ii = new ImageIcon(img, ii.getDescription());
+		Image img = facingDownII.getImage().getScaledInstance(WIDTH, WIDTH, Image.SCALE_DEFAULT);
+		facingDownII = new ImageIcon(img, facingDownII.getDescription());
+		
+		img = uncoveredII.getImage().getScaledInstance(WIDTH, WIDTH, Image.SCALE_DEFAULT);
+		uncoveredII = new ImageIcon(img, uncoveredII.getDescription());
+		
+		addMouseListener(mouseListener);
 	}
 	
 	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		
-		Image img = ii.getImage();
-		g.drawImage(img, 0, 0, this);
+		if (uncovered) {
+			Image img = uncoveredII.getImage();
+			g.drawImage(img, 0, 0, this);
+		} else {
+			Image img = facingDownII.getImage();
+			g.drawImage(img, 0, 0, this);
+		}
+	}
+	
+	private class MouseListener extends MouseInputAdapter {
+		
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			super.mouseClicked(e);
+			
+			uncovered = true;
+			repaint();
+		}
 	}
 }
